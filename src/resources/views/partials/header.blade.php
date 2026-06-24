49757a6571477b0c6c5c8287bdb9f573ad6c6ce1
Alle Palmeira <alessandro@supremodigital.com.br>
2026-06-24 11:45:26 -0300

    Atualização do menu

--- src/resources/views/partials/header.blade.php
+++ src/resources/views/partials/header.blade.php
@@ -14,7 +14,7 @@
                     <nav class="main-menu navbar-expand-md navbar-light">
                         <div class="collapse navbar-collapse clearfix" id="navbarSupportedContent">
                             <ul class="navigation menu-left clearfix">
-                                <li><a href="{{ route('home') }}">Home</a></li>
+                                <li><a href="{{ route('home') }}">TIPI 05</a></li>
                                 <li><a href="{{ route('sobre') }}">Sobre</a></li>
                                 <li class="dropdown"><a href="{{ route('cardapio.index') }}">Cardápio</a>
                                     <ul>
